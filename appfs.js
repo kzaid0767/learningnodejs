594cb74feb8ef1a4c178ed37ab9e36c49847a976
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2024-12-24 10:33:41 -0500

    intro to fs done

--- appfs.js
+++ appfs.js
@@ -1,1 +1,58 @@
-import * as fs from 'node:fs'
+import fsPromises from 'fs/promises'
+import fs from 'node:fs'
+
+// reading from file promise approach
+async function readFile(path) {
+    try {
+        const data = await fsPromises.readFile(path, 'utf-8')
+        console.log(data)
+    } catch (err) {
+        console.error('Something went wrong', err)   
+    }
+}
+
+//write to a file promise approach
+async function writeToFile(path, content) {
+    try {
+        await fsPromises.writeFile(path,content)
+        console.log('done successfully')
+    } catch (err) {
+        console.error('something went wrong:', err)
+        return
+    }
+}
+
+//writeToFile('./new.txt', 'This is some new content')
+
+//appending to file
+function addContent(path, content) {
+    fsPromises.appendFile(path, `\n${content}`, (error)=>{
+        if(error){
+            console.error('not successfull', error)
+        } else {
+            console.log('all went well')
+        }
+    })
+}
+
+//addContent('./neow.txt', 'The new content was added')
+
+
+//checking if file exist
+function checkExistence(path) {
+    fs.access(path, fs.constants.F_OK, (err) => {
+        console.log(`${path} ${err ? 'does not exist' : 'exists'}`)
+    })
+}
+
+//delete a file
+function deleteFile(path){
+    fs.unlink(path, (err) => {
+        if (err) throw err;
+        console.log(`${path} was deleted`);
+    })
+}
+
+//deleteFile('./html.pdf')
+//checkExistence('./neow.txt')
+
---
+++ osapi.js
@@ -0,0 +1,7 @@
+import os from 'node:os'
+
+console.log('The operating systems is :', os.platform())
+
+console.log('The architexture is:' ,os.arch())
+
+console.log(`The host name is ${os.hostname()}`)
